953090373e1e7ca2d620774dbbf93155be192ace
Didier <tanenpo@yahoo.com>
2025-10-01 20:03:13 -0500

    add modified setuo.sh

--- setup.sh
+++ setup.sh
@@ -14,3 +14,8 @@ sudo systemctl enable httpd
 # Get the hostname first
 HOSTNAME=$(hostname)
 echo "<html><h1><p> Welcome to Utrains.<br> You are redirected to ${HOSTNAME}</p></h1></html>" | sudo tee /var/www/html/index.html
+
+
+At work we have couple engineer running terraform code locally after making changes to the tf file. The goal is to set up some type of automation to help run those commands. 
+since we are using github action already , do some research on how we can execute those terraform command thru a ci/cd pipeline. 
+also check if there are some terraform scaning tool that can be integrated in the pipeline.
